275c162950aec0c80572d889847f0ca7689474ad
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-10-19 10:53:51 +0300

    clean script improved

--- clean.bat
+++ clean.bat
@@ -8,3 +8,14 @@ for /f "delims=" %%e in ('dir /A:D /S /B *obj^|find /i "\obj"') do @if exist "%%
 for /f "delims=" %%e in ('dir /A:D /S /B *.vs^|find /i "\.vs"') do @if exist "%%e" (@rmdir /S /Q %%e)
 
 del /S ".\Authenticator\FodyWeavers.xsd"
+
+git reflog expire --expire=1.days.ago --expire-unreachable=now --all
+if errorlevel 1 goto error
+
+git gc
+if errorlevel 1 goto error
+
+goto exit
+:error
+pause
+:exit
